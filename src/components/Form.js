56b3c5e3ada2d93a72ec9564b481247b9f19a2f2
Edward Mendoza <lalo.garibo90@gmail.com>
2020-05-12 18:05:11 -0700

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -19,13 +19,19 @@ class Form extends React.Component {
     });
   };
 
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render() {
     return (
       <form>
         <input
           type="text"
           name="firstName"
-          onChange={(event) => this.handleFirstNameChange(event)}
+          onChange={this.handleChange}
           value={this.state.firstName}
         />
         <input
